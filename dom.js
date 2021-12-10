05e9ebc5de98bf51f80ecaf48ffb6183d4365d8c
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-12-10 22:32:40 +0300

    getElementsByClassName

--- dom.js
+++ dom.js
@@ -1,17 +1,21 @@
 //document.getElementById("bio").innerHTML="Barış Arslan 2000"
 
-var intro1 = document.getElementById("intro1");
+// var intro1 = document.getElementById("intro1");
 
-var mesaj = document.getElementById("mesaj");
+// var mesaj = document.getElementById("mesaj");
 
-mesaj.innerHTML= intro1.innerHTML;
+// mesaj.innerHTML= intro1.innerHTML;
 
-var tumListeler = document.getElementsByTagName("ul");
+// var tumListeler = document.getElementsByTagName("ul");
 
-var sehirler = tumListeler[0]
+// var sehirler = tumListeler[0]
 
-tumElemanlar = sehirler.getElementsByTagName("li")
+// tumElemanlar = sehirler.getElementsByTagName("li")
 
-for(i = 0; i<tumElemanlar.length;i++){
-    alert(tumElemanlar[i].innerHTML);
-}
+// for(i = 0; i<tumElemanlar.length;i++){
+//     alert(tumElemanlar[i].innerHTML);
+// }
+
+var classElemanlari = document.getElementsByClassName("intro1");
+
+alert(classElemanlari[1].innerHTML);
